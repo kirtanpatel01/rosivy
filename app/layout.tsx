d2523e199a53da32cc5c2b029d0cca14de6a3d32
kirtan0101 <kjpatel200022@gmail.com>
2025-03-21 14:35:34 +0530

    Add Open Graph and Twitter metadata to layout for improved SEO and sharing

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,30 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 
 export const metadata: Metadata = {
-  title: "Rosivy | Home",
-  description: "Welcome to the Rosivy store",
+  title: 'Rosivy | Home',
+  description: 'Welcome to the Rosivy store',
+  openGraph: {
+    title: 'Rosivy | Home',
+    description: 'Welcome to the Rosivy store',
+    url: 'https://rosivy.vercel.app',
+    siteName: 'Rosivy',
+    images: [
+      {
+        url: '/imaegs/logo.svg',
+        width: 1200,
+        height: 630,
+        alt: 'Rosivy Store',
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Rosivy | Home',
+    description: 'Welcome to the Rosivy store',
+    images: ['/imaegs/logo.svg'], // Replace with the absolute URL of your image
+  },
 };
 
 export default function RootLayout({
