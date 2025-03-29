0be7b0fe0fd6d8d0e0c7504fd0ed1e65818e0aea
kirtan0101 <kjpatel200022@gmail.com>
2025-03-29 08:01:36 +0530

    button aligned on the bottom

--- components/Card.tsx
+++ components/Card.tsx
@@ -3,11 +3,11 @@ import { IoHeartOutline, IoHeart } from "react-icons/io5";
 
 const Card = ({ card }: { card: ProductCard }) => {
   return (
-    <div className="cont1 w-fit flex flex-col items-center gap-2 sm:gap-3">
+    <div className="cont1 w-fit flex flex-col items-center justify-between gap-2 sm:gap-3">
       <Image src={card.image} alt="green_bukey" height={200} width={200} className="rounded-lg size-36" />
       <div className="w-full flex justify-between">
         <div className="flex flex-col gap-1">
-          <span className="font-medium sm:font-semibold text-base sm:text-lg w-32 sm:w-52 text-wrap ">{card.name}</span>
+          <span className="font-medium sm:font-semibold sm:text-lg w-32 sm:w-52 text-wrap ">{card.name}</span>
           <span className="text-p9 sm:font-medium">{card.price}</span>
         </div>
         <button className="text-p6 cursor-pointer">
