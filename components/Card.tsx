7ca214a0eda649da71a22afcaf7ba1af75e16725
kirtan0101 <kjpatel200022@gmail.com>
2025-03-29 08:26:40 +0530

    Adjust width of card name span for improved layout consistency

--- components/Card.tsx
+++ components/Card.tsx
@@ -7,7 +7,7 @@ const Card = ({ card }: { card: ProductCard }) => {
       <Image src={card.image} alt="green_bukey" height={200} width={200} className="rounded-lg size-32 md:size-52" />
       <div className="w-full flex items-center justify-between border p-2 sm:p-4 rounded-lg border-p4 bg-p2/10">
         <div className="flex flex-col gap-1">
-          <span className="font-medium md:font-semibold s text-sm md:text-lg w-32 md:w-32 truncate ">{card.name}</span>
+          <span className="font-medium md:font-semibold s text-sm md:text-lg w-24 md:w-32 truncate ">{card.name}</span>
           <span className="text-p9 md:font-medium text-xs">{card.price}</span>
         </div>
         <button className="text-p6 cursor-pointer">
