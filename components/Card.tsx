691ce9ac91adfd19f8b3ee9b87b74c3b7220d1d8
kirtan0101 <kjpatel200022@gmail.com>
2025-03-29 08:32:50 +0530

    Refactor Card component and global styles; remove unnecessary padding and adjust price span for improved layout

--- components/Card.tsx
+++ components/Card.tsx
@@ -7,8 +7,8 @@ const Card = ({ card }: { card: ProductCard }) => {
       <Image src={card.image} alt="green_bukey" height={200} width={200} className="rounded-lg size-36 md:size-52" />
       <div className="w-full flex items-center justify-between border p-2 sm:p-4 rounded-lg border-p4 bg-p2/10">
         <div className="flex flex-col gap-1">
-          <span className="font-medium md:font-semibold s text-sm md:text-lg w-28 md:w-32 truncate ">{card.name}</span>
-          <span className="text-p9 md:font-medium text-xs">{card.price}</span>
+          <span className="font-medium md:font-semibold md:text-lg w-28 md:w-32 truncate ">{card.name}</span>
+          <span className="text-p9 md:font-medium">{card.price}</span>
         </div>
         <button className="text-p6 cursor-pointer">
           {true ? <IoHeart className="size-5 md:size-7" /> : <IoHeartOutline className="size-5 md:size-7" />}
