a1382a78a5f321f4d6f85fe6672c34e20ed17c13
kirtan0101 <kjpatel200022@gmail.com>
2025-03-29 08:14:59 +0530

    Update layout and styling for Card component; adjust responsive design for better UI

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import { products } from "@/lib/constants";
 export default function Home() {
   return (
     <div className="cont bg-p1">
-      <ul className="flex flex-wrap justify-center gap-4 sm:gap-8 p-4 sm:p-8">
+      <ul className="flex flex-wrap justify-center gap-4 md:gap-8 p-4 md:p-8">
         {products.map((card) => (
           <Card card={card} key={card.id} />
         ))}
--- components/Card.tsx
+++ components/Card.tsx
@@ -3,15 +3,15 @@ import { IoHeartOutline, IoHeart } from "react-icons/io5";
 
 const Card = ({ card }: { card: ProductCard }) => {
   return (
-    <div className="cont1 w-fit flex flex-col items-center justify-between gap-2 sm:gap-3">
-      <Image src={card.image} alt="green_bukey" height={200} width={200} className="rounded-lg size-36" />
-      <div className="w-full flex justify-between">
+    <div className="cont1 w-fit flex flex-col items-center justify-between gap-2 md:gap-3">
+      <Image src={card.image} alt="green_bukey" height={200} width={200} className="rounded-lg size-36 md:size-52" />
+      <div className="w-full flex items-center justify-between border p-2 rounded-lg border-p4">
         <div className="flex flex-col gap-1">
-          <span className="font-medium sm:font-semibold sm:text-lg w-32 sm:w-52 text-wrap ">{card.name}</span>
-          <span className="text-p9 sm:font-medium">{card.price}</span>
+          <span className="font-medium md:font-semibold md:text-lg w-32 md:w-32 truncate ">{card.name}</span>
+          <span className="text-p9 md:font-medium">{card.price}</span>
         </div>
         <button className="text-p6 cursor-pointer">
-          {true ? <IoHeart className="size-5 sm:size-7" /> : <IoHeartOutline className="size-5 sm:size-7" />}
+          {true ? <IoHeart className="size-5 md:size-7" /> : <IoHeartOutline className="size-5 md:size-7" />}
         </button>
       </div>
 
